3d99f3346614d06492254b1afff6e803191b62fe
smerapala <smerapala@gmail.com>
2022-08-23 16:29:36 -0400

    Add request code

--- api.py
+++ api.py
@@ -1,7 +1,15 @@
 # Groups companies by industry and ranks top companies by stock performance
 
+import requests
+
+
 apiKey = ''
 with open('key.txt') as f:
   apiKey = f.read()
 
-print(apiKey)
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey={apiKey}'
+r = requests.get(url)
+data = r.json()
+
+print(data)
