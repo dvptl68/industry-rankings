08cb7000c7c326b9d0cc41603416f89bae0c5214
Dev Patel <dvptl68@gmail.com>
2022-08-25 16:04:15 -0400

    Add user input

--- api.py
+++ api.py
@@ -2,13 +2,17 @@
 import requests
 import json
 
+# API key for Alpha Vantage
 apiKey = ''
 with open('key.txt') as f:
   apiKey = f.read()
 
-url = f'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&topics=technology&limit=50&apikey={apiKey}'
+# API call to Alpha Vantage
+ticker = input('Enter a company stock abbreviation: ')
+url = f'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={ticker}&topics=technology&limit=50&apikey={apiKey}'
 r = requests.get(url)
 data = r.json()
 
+# Prints the data to a file
 with open('out.json', 'w+') as f:
   f.write(json.dumps(data, indent=4))
